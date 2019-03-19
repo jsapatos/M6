5a07169e8305c8f981ebff949930c32e1e590b16
jsapatos <33329819+jsapatos@users.noreply.github.com>
2019-03-19 19:20:07 +0100

    Add files via upload

---
+++ UF3/eXist/verproductos.java
@@ -0,0 +1,32 @@
+package xqj;
+
+import javax.xml.xquery.XQConnection;
+import javax.xml.xquery.XQDataSource;
+import javax.xml.xquery.XQException;
+import javax.xml.xquery.XQPreparedExpression;
+import javax.xml.xquery.XQResultItem;
+import javax.xml.xquery.XQResultSequence;
+import net.xqj.exist.ExistXQDataSource;
+
+public class verproductos {
+	public static void main(String[] args){
+		try{
+			XQDataSource server = new ExistXQDataSource();
+			server.setProperty ("serverName", "192.168.56.102");
+			server.setProperty ("port","8080");
+			server.setProperty ("user","admin");
+			server.setProperty ("password","austria");
+			XQConnection conn = server.getConnection();
+			XQPreparedExpression consulta;XQResultSequence resultado;
+			consulta = conn.prepareExpression ("for $pr in doc('nueva/productos.xml')/productos/produc return $pr");
+			resultado = consulta.executeQuery();
+			while (resultado.next()) {
+				System.out.println(resultado.getItemAsString(null));
+			}
+			conn.close();
+			} 
+		catch (XQException ex) {
+			System.out.println("Error al operar "+ex.getMessage());
+		}
+	}
+}
